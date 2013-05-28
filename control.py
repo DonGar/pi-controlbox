32860da866902771352d96cc7e1cb0e67c801ca6
Don Garrett <dgarrett@tongs.dongar.net>
2013-05-27 22:44:54 -0700

    Move URL notifications into background threads.
    Also, revamp which buttons exist, what URLs they are mapped too, and remove
    display lights (for now).

--- control.py
+++ control.py
@@ -1,48 +1,40 @@
 #!/usr/bin/python
 
 import time
-import urllib
 
 import RPi.GPIO as GPIO
 
+import notify
+
 BUTTON_URL = {
-  7: 'http://www:8080/button?id=system1',
-  11: 'http://www:8080/button?id=system2',
-  13: 'http://www:8080/button?id=system3',
-  15: 'http://www:8080/button?id=system4',
+  7: 'http://www:8080/button?id=control_red',
+  11: 'http://www:8080/button?id=control_black',
   8: 'http://www:8080/button?id=doorbell',
 }
 
 BUTTON_PINS = BUTTON_URL.keys()
-BUTTON_LIGHT = {7: 10, 11: 16, 13: 18, 15: 22}
-LIGHT_PINS = BUTTON_LIGHT.values()
-
 BUTTON_DOWN = {}
-LIGHT_ON = {10: False, 11: False, 18: False, 22: False}
 
-def button_pushed(pin):
-  print 'Button down!: %d' % pin
+notifier = notify.Notifier()
 
-  on_off = ''
 
-  if pin in BUTTON_LIGHT:
-    light_pin = BUTTON_LIGHT[pin]
+def check_buttons():
+  # If the pin transitioned to low since we checked, it's been pushed.
+  for pin in BUTTON_PINS:
 
-    # Request a change to the oposite of the current state.
-    if light_pin:
-      on_off = '&action=off'
-    else:
-      on_off = '&action=on'
+    read_down = GPIO.input(pin)
 
-    # Notice that GPIO setting is False for light, True for dark.
-    gpio_setting = LIGHT_ON[light_pin]
-    GPIO.output(light_pin, gpio_setting)
-    LIGHT_ON[light_pin] = not gpio_setting
+    # Pin 7 is normally high, transitions to low when pushed.
+    if pin == 7:
+      read_down = not read_down
 
-  url = BUTTON_URL[pin] + on_off
-  f = urllib.urlopen(url)
-  f.read()
-  f.close()
+    if read_down != BUTTON_DOWN.get(pin, False):
+      BUTTON_DOWN[pin] = read_down
+
+      if read_down:
+        print 'Button down!: %d' % pin
+        notifier.notify_url(BUTTON_URL[pin])
+
 
 def main():
   GPIO.setmode(GPIO.BOARD)
@@ -51,24 +43,9 @@ def main():
   for pin in BUTTON_PINS:
     GPIO.setup(pin, GPIO.IN)
 
-  for pin in LIGHT_PINS:
-    GPIO.setup(pin, GPIO.OUT)
-    GPIO.output(pin, True)
-    LIGHT_ON[pin] = False
-
   while True:
-    # If the pin transitioned to low since we checked, it's been pushed.
-    for pin in BUTTON_PINS:
-
-      read_down = GPIO.input(pin)
-
-      if read_down != BUTTON_DOWN.get(pin, False):
-        BUTTON_DOWN[pin] = read_down
-
-        if read_down:
-          button_pushed(pin)
-
+    check_buttons()
     time.sleep(0.05)
 
 if __name__ == "__main__":
-    main()
+  main()
